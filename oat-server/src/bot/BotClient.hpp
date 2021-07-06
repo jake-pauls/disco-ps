64961d6249ce1e424706c5d77f7e32c4e91344f1
jake-pauls <jakepauls99@gmail.com>
2021-07-05 22:34:05 -0700

    Small housekeeping changes after dockerization

--- oat-server/src/bot/BotClient.hpp
+++ oat-server/src/bot/BotClient.hpp
@@ -10,7 +10,7 @@ public:
     using SleepyDiscord::DiscordClient::DiscordClient;
     void onMessage(SleepyDiscord::Message message) override {
         std::cout << "[onMessage]: Triggered Hello WebSocket." << std::endl;
-        if (message.startsWith("whcg hello"))
+        if (message.startsWith("Hello bot!"))
             sendMessage(message.channelID, "Hello " + message.author.username);
     }
 }; 
